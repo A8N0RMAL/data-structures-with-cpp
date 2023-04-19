ce7e2a553b2e84bf4b1858fab1b0992d0eeb984d
A8N0RMAL <masabnormal@gmail.com>
2023-04-19 19:22:10 +0200

    data-structures-with-c++

---
+++ BinarySearch/main.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include <bits/stdc++.h>
+#define fast ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+using namespace std;
+int binarySearch(int arr[], int l, int h, int element)
+{
+	while (l <= h) {
+
+		int m = (l + h) / 2;//both=>m = l + (r - l) / 2 both=>m = (2 l + r -l)/2
+
+
+		if (arr[m] == element)
+			return m;
+
+		if (arr[m] > element)
+
+		h = m - 1;
+
+		else
+
+		l = m + 1;
+
+	}
+
+	return -1;
+}
+
+int main()
+{
+	int arr[] = { 2, 3, 4, 10, 40 };
+	int n = sizeof(arr) / sizeof(arr[1]);
+
+	int num;
+	cout << "Enter an Integer :";
+	cin >> num;
+
+	int result = binarySearch(arr, 0, n - 1, num);
+	if(result == -1)
+		cout << "The Number : (" << num << ") Was Not Found." << endl;
+	else
+		cout << "The Number : (" << arr[result] << ") Was Found At Index : (" << result << ")" << endl;
+
+	return 0;
+}
